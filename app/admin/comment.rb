d78990efa47c58de5738435d9780faf12048c5cd
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:21:11 +0000

    Whitelist columns in Comment for ActiveAdmin

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Comment do
 
+ permit_params :users_id, :description, :photos_id, :friend_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
